e92baf0a8b9293ec5073cb4f549c2424fbbd9a83
a2804 <kalysbekovadilet4@gmail.com>
2022-09-16 15:03:55 +0600

    dz1_4month

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+function Portfolio () {
+    return(
+        <h1 className="port">Portfolio</h1>
+    )
+}
+
+function AboutUs () {
+  return (
+      <h1 className="about">AboutUs</h1>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <Portfolio/>
+        <AboutUs/>
     </div>
   );
 }
